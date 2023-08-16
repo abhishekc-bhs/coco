f7378ad4bfa2e28063861133e2b7c0e7f47b8d74
Abhishek Choudhury <115447569+abhishekc-bhs@users.noreply.github.com>
2023-08-16 15:19:10 +0530

    first commit

---
+++ src/routes/coco/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  let catFactString = "";
+
+  async function getCatFact() {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/json");
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    await fetch("https://catfact.ninja/fact?max_length=140", requestOptions)
+      .then((response) => response.json())
+      .then((result) => (catFactString = result.fact))
+      .catch((error) => console.log("error", error));
+  }
+</script>
+
+<h1>{catFactString}</h1>
+<button
+  on:click={getCatFact}
+  class="px-8 py-3 bg-blue-800 text-white rounded-3xl">Get Cat Fact</button
+>
